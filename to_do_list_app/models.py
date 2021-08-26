797c1f5aa1f7af14ab59345ee8d5acbe133f1c59
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-08-26 15:26:00 +0900

    Add created field in Item

--- to_do_list_app/models.py
+++ to_do_list_app/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Item(models.Model):
     content = models.CharField(max_length = 255)
     is_done = models.BooleanField(default = False)
+    created = models.DateTimeField(default = timezone.now())
     def __str__(self):
         return self.content
